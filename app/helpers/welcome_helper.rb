562ada15c1a82f0a2b38a862fc4cb2106ef3a941
victorsouza19 <victor.cristofalo@hotmail.com>
2021-12-28 22:06:59 -0300

    feat: add helper - check_environment helper - add pry_rails gem

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,13 @@
 module WelcomeHelper
+  def check_environment
+
+    if Rails.env.development?
+      "Development"
+    elsif Rails.env.production?
+      "Production"
+    else
+      "Test"
+    end
+    
+  end
 end
