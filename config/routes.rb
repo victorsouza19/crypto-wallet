1bf3c9889f217b489110ea81d990eee34e20e119
victorsouza19 <victor.cristofalo@hotmail.com>
2021-12-28 21:34:38 -0300

    feat: custom routes - create custom routes at routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
+  root to: 'welcome#index'
+
   resources :coins
+  # get '/coins', to: 'coins#index'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
