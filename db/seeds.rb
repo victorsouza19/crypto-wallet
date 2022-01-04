473c2a51a4faec268f8b90ee402452e282e3a4f1
victorsouza19 <victor.cristofalo@hotmail.com>
2022-01-03 22:37:49 -0300

    feat: first seed - create a seed to configure database during the development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+  title: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Logo.png"
+)
+
+Coin.create!(
+  title: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://www.ceiconsultoria.com.br/wp-content/uploads/ethereum.png"
+)
+
+Coin.create!(
+  title: "Dash",
+  acronym: "DASH",
+  url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"
+)
+
+Coin.create!(
+  title: "Dogecoin",
+  acronym: "DOGE",
+  url_image: "https://bitpreco.com/assets/img/coins/dogecoin/bitpreco-moeda-dogecoin.svg"
+)
