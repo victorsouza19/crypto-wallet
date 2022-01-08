5b9b8123e1d8dcde465f30b815facc26b59cc07f
victorsouza19 <victor.cristofalo@hotmail.com>
2022-01-07 22:45:20 -0300

    fix: change model column name - MiningType model change of the column 'name' to 'description' to prevent errors with rails alias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_004754) do
+ActiveRecord::Schema.define(version: 2022_01_08_012901) do
 
   create_table "coins", force: :cascade do |t|
     t.string "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_01_08_004754) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
