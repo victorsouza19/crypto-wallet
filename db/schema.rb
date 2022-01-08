e9022f50cd382eefcf012bba82e4c4ecc163d0b2
victorsouza19 <victor.cristofalo@hotmail.com>
2022-01-07 21:56:30 -0300

    feat: MiningType - add MiningType model with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_232528) do
+ActiveRecord::Schema.define(version: 2022_01_08_004754) do
 
   create_table "coins", force: :cascade do |t|
     t.string "title"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
