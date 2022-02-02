034dbef322d35cb7d8c707e026ac35461f797d09
victorsouza19 <victor.cristofalo@hotmail.com>
2022-02-01 23:25:29 -0300

    wip: add cookies and sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:course] = "Ruby on Rails (:"
+    session[:username] = "Victor Souza"
     @my_name = params[:name]
   end
 end
